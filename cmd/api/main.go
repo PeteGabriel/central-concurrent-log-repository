03a729c68f3e7317e20890b49fa7947d52837f9a
Pedro Gabriel <pedrogabriel@protonmail.ch>
2021-08-05 09:57:11 +0200

    Add settings data type

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"petegabriel/central-concurrent-log/pkg/config"
+)
 
 func main() {
-	fmt.Println("Hello from central project")
+	s := config.New()
+	l, err := net.Listen("tcp", s.Host+s.Port)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(l.Addr().String())
+
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Found a stranger")
+		c.Write([]byte("Hello stranger"))
+	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module petegabriel/central-concurrent-log
 
 go 1.16
 
-require (
-	github.com/labstack/echo/v4 v4.5.0 // indirect
-	github.com/spf13/viper v1.8.1 // indirect
-)
+require github.com/spf13/viper v1.8.1 // indirect
