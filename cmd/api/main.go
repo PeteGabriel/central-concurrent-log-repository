03331c4f71df78ac245d2ec9f9b88e6efc041547
PeteGabriel <pedrogabriel@protonmail.ch>
2021-08-06 11:49:03 +0200

    Add handler for new clients.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,13 +18,20 @@ func main() {
 
 	fmt.Println(l.Addr().String())
 
-	defer l.Close()
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(l)
 
 	amnt, err := strconv.Atoi(s.Clients)
 	if err != nil {
 		log.Println("Accepting 5 clients")
 		amnt = 5 // default value of accepted clients
 	}
+	//use this buffered channel to control how many
+	//clients can connect to us simultaneously
 	sem := make(chan int, amnt)
 
 	for {
@@ -33,7 +40,6 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-
-		go handlers.HandleNewClient(c, sem)
+		go handlers.HandleNewClient(c, sem, s)
 	}
 }
--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,19 +1,54 @@
 package handlers
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"petegabriel/central-concurrent-log/pkg/config"
+	"strconv"
+	"strings"
 )
 
-func HandleNewClient(c net.Conn, sem chan int) {
+const (
+	TerminateCmd = "TERMINATE"
+	TerminateCmdMsg = "Terminating process.\nClosing connection.\n"
+)
 
-	if len(sem) == 2 {
+
+//HandleNewClient receive a new connection to a client and a channel
+//where it can check if client can connect to the application or if
+//connection need to be released due to overflow of clients.
+func HandleNewClient(c net.Conn, sem chan int, st *config.Settings) {
+
+	amount, err := strconv.Atoi(st.Clients)
+	if err != nil {
+		panic(err)
+	}
+	if len(sem) == amount {
 		c.Write([]byte("Cannot accept more clients.."))
 		c.Close()
 		return
 	}
 	sem <- 1
-	fmt.Println("Sending msg...")
-	c.Write([]byte("Hello from goroutine"))
+
+	for {
+		cmd, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println("Error communicating with client. Closing connection.")
+			c.Close()
+			<-sem
+			return
+		}
+		//handle terminate cmd
+		if strings.TrimSpace(cmd) == TerminateCmd {
+			c.Write([]byte(TerminateCmdMsg))
+			c.Close()
+			<-sem
+			return
+		}
+
+		c.Write([]byte("Hello from TCP server\n"))
+	}
+
 
 }
